be6093641f8eaa0d37e9a1e989da0ec8a85f0e1b
Andrew Deur <andrewrdeur@gmail.com>
2023-12-06 00:35:00 -0500

    BERT eval batch size fix

--- msmarco_dataloader.py
+++ msmarco_dataloader.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import torch
+from functools import partial
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from transformers import BertTokenizer, BertForSequenceClassification
@@ -64,7 +65,7 @@ class MSMARCODataset(Dataset):
         }
 
 
-def collate_fn(batch):
+def collate_fn(batch, tokenizer):
     query_input_ids = [item['query_input_ids'] for item in batch]
     query_attention_masks = [item['query_attention_mask'] for item in batch]
     passage_input_ids = [item['passage_input_ids'] for item in batch]
@@ -73,7 +74,6 @@ def collate_fn(batch):
 
     # Find the maximum number of passages and maximum passage length in this batch
     max_passage_count = max(len(p) for p in passage_input_ids)
-    
     padded_passage_input_ids = []
     padded_passage_attention_masks = []
 
@@ -104,58 +104,63 @@ def collate_fn(batch):
 
 def create_data_loader(dataframe, tokenizer, batch_size, candidate_document_set_size, max_token_length=512):
     dataset = MSMARCODataset(dataframe, tokenizer, candidate_document_set_size, max_token_length)
-    return DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn)
+
+    collate_fn_with_tokenizer = partial(collate_fn, tokenizer=tokenizer)
+    return DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn_with_tokenizer)
 
 
+def main():
+    # Example usage
+    tokenizer = BertTokenizer.from_pretrained('Capreolus/bert-base-msmarco')
+    df = pd.read_csv(r"C:\Users\Andrew Deur\Documents\NYU\DS-GA 1011 NLP\Project\filtered_top1000_dev_with_labels_bmscore.tsv")
+    data_loader = create_data_loader(df, tokenizer, batch_size=32, candidate_document_set_size=20)
 
-# Example usage
-tokenizer = BertTokenizer.from_pretrained('Capreolus/bert-base-msmarco')
-df = pd.read_csv(r"C:\Users\Andrew Deur\Documents\NYU\DS-GA 1011 NLP\Project\filtered_top1000_dev_with_labels_bmscore.tsv")
-data_loader = create_data_loader(df, tokenizer, batch_size=32, candidate_document_set_size=20)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f"Using device: {device}")
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Using device: {device}")
+    model_name = "Capreolus/bert-base-msmarco"
+    tokenizer = BertTokenizer.from_pretrained(model_name)
+    model = BertForSequenceClassification.from_pretrained(model_name)
+    model.to(device)
+    model.eval()  # Set the model to evaluation mode
+
+    scores = []
+    with torch.no_grad():  # Disable gradient computation
+        
+        for batch in tqdm(data_loader):
+            query_input_ids = batch['query_input_ids'].to(device)
+            query_attention_mask = batch['query_attention_mask'].to(device)
+
+            batch_scores = []
+            for p_input_ids, p_attention_mask in zip(batch['passage_input_ids'], batch['passage_attention_mask']):
+                
+                # Ensure the tensors are of the same batch size
+                min_batch_size = min(query_input_ids.size(0), p_input_ids.size(0))
+                query_input_ids_resized = query_input_ids[:min_batch_size]
+                query_attention_mask_resized = query_attention_mask[:min_batch_size]
+                p_input_ids_resized = p_input_ids[:min_batch_size].to(device)
+                p_attention_mask_resized = p_attention_mask[:min_batch_size].to(device)
+
+                # Concatenate along the sequence length dimension
+                combined_input_ids = torch.cat([query_input_ids_resized, p_input_ids_resized], dim=1)
+                combined_attention_mask = torch.cat([query_attention_mask_resized, p_attention_mask_resized], dim=1)
+
+                # Ensure the combined sequence does not exceed the maximum length
+                max_length = model.config.max_position_embeddings
+                if combined_input_ids.size(1) > max_length:
+                    combined_input_ids = combined_input_ids[:, :max_length]
+                    combined_attention_mask = combined_attention_mask[:, :max_length]
+
+                # Run model inference
+                outputs = model(input_ids=combined_input_ids, attention_mask=combined_attention_mask)
+                
+                # Extract relevance scores
+                probabilities = torch.softmax(outputs.logits, dim=1)
+                batch_scores = probabilities[:, 1]  # Assuming label '1' is relevant
+
+                # Flatten the batch_scores tensor and extend the main scores list
+                scores.extend(batch_scores.flatten().tolist())
 
 
-model_name = "Capreolus/bert-base-msmarco"
-tokenizer = BertTokenizer.from_pretrained(model_name)
-model = BertForSequenceClassification.from_pretrained(model_name)
-model.to(device)
-model.eval()  # Set the model to evaluation mode
-
-scores = []
-with torch.no_grad():  # Disable gradient computation
-    
-    for batch in tqdm(data_loader):
-        query_input_ids = batch['query_input_ids'].to(device)
-        query_attention_mask = batch['query_attention_mask'].to(device)
-
-        batch_scores = []
-        for p_input_ids, p_attention_mask in zip(batch['passage_input_ids'], batch['passage_attention_mask']):
-            
-            # Ensure the tensors are of the same batch size
-            min_batch_size = min(query_input_ids.size(0), p_input_ids.size(0))
-            query_input_ids_resized = query_input_ids[:min_batch_size]
-            query_attention_mask_resized = query_attention_mask[:min_batch_size]
-            p_input_ids_resized = p_input_ids[:min_batch_size].to(device)
-            p_attention_mask_resized = p_attention_mask[:min_batch_size].to(device)
-
-            # Concatenate along the sequence length dimension
-            combined_input_ids = torch.cat([query_input_ids_resized, p_input_ids_resized], dim=1)
-            combined_attention_mask = torch.cat([query_attention_mask_resized, p_attention_mask_resized], dim=1)
-
-            # Ensure the combined sequence does not exceed the maximum length
-            max_length = model.config.max_position_embeddings
-            if combined_input_ids.size(1) > max_length:
-                combined_input_ids = combined_input_ids[:, :max_length]
-                combined_attention_mask = combined_attention_mask[:, :max_length]
-
-            # Run model inference
-            outputs = model(input_ids=combined_input_ids, attention_mask=combined_attention_mask)
-            
-            # Extract relevance scores
-            probabilities = torch.softmax(outputs.logits, dim=1)
-            batch_scores = probabilities[:, 1]  # Assuming label '1' is relevant
-
-            # Flatten the batch_scores tensor and extend the main scores list
-            scores.extend(batch_scores.flatten().tolist())
+if __name__ == '__main__':
+    main()
