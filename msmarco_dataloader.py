de389c3e1ac42f7e133ad5cf35b357be2042ec64
Andrew Deur <andrewrdeur@gmail.com>
2023-12-06 09:45:14 -0500

    Remove passage truncation from dataloader

--- msmarco_dataloader.py
+++ msmarco_dataloader.py
@@ -1,39 +1,27 @@
 import pandas as pd
 import torch
-from functools import partial
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 from transformers import BertTokenizer, BertForSequenceClassification
-from datasets import Dataset as HFDataset
+from datasets import Dataset as HFDataset # Rename to avoid conflict with PyTorch Datasets
 from tqdm.auto import tqdm
 
 class MSMARCODataset(Dataset):
-    def __init__(self, dataframe, tokenizer, candidate_document_set_size, max_token_length=512):
+    
+    def __init__(self, dataframe, tokenizer, max_token_length=512):
+        
         self.dataframe = dataframe
         self.tokenizer = tokenizer
         self.max_token_length = max_token_length
-        self.candidate_documents_size = candidate_document_set_size
-        self.grouped = self._group_data()
-
-    def _group_data(self):
-        # Group by 'qid' and select top N docs based on BM25 score
-        return self.dataframe.groupby('qid', group_keys=False)\
-            .apply(lambda x: x.nlargest(self.candidate_documents_size, 'bm25_score'))
 
     def __len__(self):
-        return len(self.grouped['qid'].unique())
-    
+        return len(self.dataframe)
+
     def __getitem__(self, idx):
-        # Map idx to the corresponding qid
-        unique_qids = self.grouped['qid'].unique()
-        if idx >= len(unique_qids):
-            raise IndexError("Index out of bounds")
-
-        query_id = unique_qids[idx]
-
-        # Get the group corresponding to this query ID
-        group = self.grouped[self.grouped['qid'] == query_id]
-        query_text = group['query'].iloc[0]  # The query is the same for all passages in the group
+        
+        row = self.dataframe.iloc[idx]
+        query_text = str(row['query'])
+        passage_text = str(row['passage'])
 
         query_encoding = self.tokenizer(
             query_text,
@@ -44,76 +32,69 @@ class MSMARCODataset(Dataset):
             return_tensors="pt"
         )
 
-        passages = group['passage'].tolist()
-        passage_encodings = [self.tokenizer(
-            passage,
+        passage_encoding = self.tokenizer(
+            passage_text,
             add_special_tokens=True,
             padding='max_length',
             truncation=True,
             max_length=self.max_token_length,
             return_tensors="pt"
-        ) for passage in passages]
+        )
 
-        labels = torch.tensor(group['relevance'].tolist())
+        label = torch.tensor(row['relevance'])
 
         return {
             'query_input_ids': query_encoding['input_ids'].squeeze(0),
             'query_attention_mask': query_encoding['attention_mask'].squeeze(0),
-            'passage_input_ids': [pe['input_ids'].squeeze(0) for pe in passage_encodings],
-            'passage_attention_mask': [pe['attention_mask'].squeeze(0) for pe in passage_encodings],
-            'labels': labels
+            'passage_input_ids': passage_encoding['input_ids'].squeeze(0),
+            'passage_attention_mask': passage_encoding['attention_mask'].squeeze(0),
+            'label': label
         }
 
 
-def collate_fn(batch, tokenizer):
-    query_input_ids = [item['query_input_ids'] for item in batch]
-    query_attention_masks = [item['query_attention_mask'] for item in batch]
-    all_passage_input_ids = []
-    all_passage_attention_masks = []
+def collate_fn(batch):
+    max_length = 512  # Adjust based on your model's max length
+    input_ids_batch = []
+    attention_mask_batch = []
+    labels_batch = []
 
     for item in batch:
-        # Concatenate all passages for each query
-        concatenated_passages_input_ids = torch.cat(item['passage_input_ids'], dim=0)
-        concatenated_passages_attention_mask = torch.cat(item['passage_attention_mask'], dim=0)
+        # Concatenate the query and passage input ids and attention masks
+        combined_input_ids = torch.cat([item['query_input_ids'], item['passage_input_ids']])[:max_length]
+        combined_attention_mask = torch.cat([item['query_attention_mask'], item['passage_attention_mask']])[:max_length]
 
-        # Padding if necessary to match the max sequence length
-        max_length = tokenizer.model_max_length
-        if concatenated_passages_input_ids.size(0) < max_length:
-            required_padding = max_length - concatenated_passages_input_ids.size(0)
-            concatenated_passages_input_ids = torch.cat([
-                concatenated_passages_input_ids,
-                torch.full((required_padding,), tokenizer.pad_token_id, dtype=torch.long)
-            ])
-            concatenated_passages_attention_mask = torch.cat([
-                concatenated_passages_attention_mask,
-                torch.zeros(required_padding, dtype=torch.long)
-            ])
+        # Calculate padding length for this concatenated pair
+        padding_length = max_length - combined_input_ids.size(0)
 
-        all_passage_input_ids.append(concatenated_passages_input_ids)
-        all_passage_attention_masks.append(concatenated_passages_attention_mask)
+        # Pad the concatenated input ids and attention masks to max_length
+        padded_input_ids = torch.cat([combined_input_ids, torch.zeros(padding_length, dtype=torch.long)])
+        padded_attention_mask = torch.cat([combined_attention_mask, torch.zeros(padding_length, dtype=torch.long)])
 
-    query_input_ids = torch.stack(query_input_ids)
-    query_attention_masks = torch.stack(query_attention_masks)
-    all_passage_input_ids = torch.stack(all_passage_input_ids)
-    all_passage_attention_masks = torch.stack(all_passage_attention_masks)
+        input_ids_batch.append(padded_input_ids)
+        attention_mask_batch.append(padded_attention_mask)
+        labels_batch.append(item['label'])
+
+    # Convert lists to tensors
+    input_ids_batch = torch.stack(input_ids_batch)
+    attention_mask_batch = torch.stack(attention_mask_batch)
+    labels_batch = torch.stack(labels_batch)
 
     return {
-        'query_input_ids': query_input_ids,
-        'query_attention_mask': query_attention_masks,
-        'passage_input_ids': all_passage_input_ids,
-        'passage_attention_mask': all_passage_attention_masks
+        'input_ids': input_ids_batch,
+        'attention_mask': attention_mask_batch,
+        'labels': labels_batch
     }
 
-def create_data_loader(dataframe, tokenizer, batch_size, candidate_document_set_size, max_token_length=512):
-    dataset = MSMARCODataset(dataframe, tokenizer, candidate_document_set_size, max_token_length)
-    collate_fn_with_tokenizer = partial(collate_fn, tokenizer=tokenizer)
-    return DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn_with_tokenizer)
+
+def create_data_loader(dataframe, tokenizer, batch_size, max_token_length=512):
+    dataset = MSMARCODataset(dataframe, tokenizer, max_token_length)
+    return DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, num_workers=0)
 
 
 def main():
     tokenizer = BertTokenizer.from_pretrained('Capreolus/bert-base-msmarco')
     df = pd.read_csv(r"C:\Users\Andrew Deur\Documents\NYU\DS-GA 1011 NLP\Project\filtered_top1000_dev_with_labels_bmscore.tsv")
-    data_loader = create_data_loader(df, tokenizer, batch_size=32, candidate_document_set_size=20)
+    data_loader = create_data_loader(df, tokenizer, batch_size=32)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(f"Using device: {device}")
@@ -128,23 +109,12 @@ def main():
     with torch.no_grad():  # Disable gradient computation
         
         for batch in tqdm(data_loader):
-            query_input_ids = batch['query_input_ids'].to(device)
-            query_attention_mask = batch['query_attention_mask'].to(device)
-            passage_input_ids = batch['passage_input_ids'].to(device)
-            passage_attention_mask = batch['passage_attention_mask'].to(device)
-
-            # Concatenate along the sequence length dimension
-            combined_input_ids = torch.cat([query_input_ids, passage_input_ids], dim=1)
-            combined_attention_mask = torch.cat([query_attention_mask, passage_attention_mask], dim=1)
-
-            # Ensure the combined sequence does not exceed the maximum length
-            max_length = model.config.max_position_embeddings
-            if combined_input_ids.size(1) > max_length:
-                combined_input_ids = combined_input_ids[:, :max_length]
-                combined_attention_mask = combined_attention_mask[:, :max_length]
+            input_ids = batch['input_ids'].to(device)
+            attention_mask = batch['attention_mask'].to(device)
+            labels = batch['labels'].to(device)
 
             # Run model inference
-            outputs = model(input_ids=combined_input_ids, attention_mask=combined_attention_mask)
+            outputs = model(input_ids=input_ids, attention_mask=attention_mask)
 
             # Extract relevance scores
             probabilities = torch.softmax(outputs.logits, dim=1)
